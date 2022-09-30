ee67073d2b05b8aefa54c1220a79d6486acaff8e
Alex-Flores <55876853+Alex-Flores@users.noreply.github.com>
2022-09-30 16:19:53 -0300

    Add files via upload

---
+++ 03 - Renderizado Pelis/app.js
@@ -0,0 +1,12 @@
+const appWeb = new Vue({
+    el: '#app',
+    data: {
+        lista: peliculas,
+        si_style: 'fa-solid fa-lg fa-heart like',
+        no_style: 'fa-solid fa-lg fa-heart',
+        isactive: true
+    },
+    methods: {
+
+    }
+})
